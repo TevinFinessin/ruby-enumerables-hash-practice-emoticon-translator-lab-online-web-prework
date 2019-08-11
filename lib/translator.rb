c7618ceb989f33602239d9f811045315b9d38721
benevolent-demo-1612 <tevinsherman@yahoo.com>
2019-08-11 00:14:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,18 @@
 # require modules here
-
-def load_library
+require 'yaml'
+require 'pry'
+def load_library(file_path)
   # code goes here
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticon|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+end
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
